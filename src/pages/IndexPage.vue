5b72c0665184c6d1888a88e8b6fe2fe24e67b3c0
Gerihh <horvathgergo1208@gmail.com>
2024-02-02 15:42:26 +0100

    ...

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,11 @@
 <template>
-  <ExampleComponent />
+  <h1>home</h1>
 </template>
 
 <script>
-import ExampleComponent from "src/components/ExampleComponent.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
-  components: { ExampleComponent },
 });
 </script>
